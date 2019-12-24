5273cdfb648e9d9ec05130befd8741f9542f3858
Alex <35946230+iceshellwang@users.noreply.github.com>
2019-12-24 14:36:07 +1100

    Add files via upload

---
+++ build/precache-manifest.78fa4e6031320a33901ec43d116e5218.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f15ffbe3cdf96be070cc501b7731d48d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ec069346b00f94b1597a",
+    "url": "/static/css/main.db612ab2.chunk.css"
+  },
+  {
+    "revision": "e252919a658a8c2497ff",
+    "url": "/static/js/2.a2ed4172.chunk.js"
+  },
+  {
+    "revision": "ec069346b00f94b1597a",
+    "url": "/static/js/main.f9beb1dd.chunk.js"
+  },
+  {
+    "revision": "1f3c89d7d8e9d2ccca39",
+    "url": "/static/js/runtime-main.e89362ac.js"
+  },
+  {
+    "revision": "32a070ef377db93a37d0732e1a727bac",
+    "url": "/static/media/bg.32a070ef.jpg"
+  }
+]);
---
+++ src/components/Titles.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+const Titles = () => (
+    <div>
+        <h1 className="title-container__title">Weather Forcast</h1>
+        <h3 className="title-container__subtitle">Return temperature and conditions</h3>
+    </div>
+);
+
+export default Titles;
